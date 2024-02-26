4372c75206c9193e776c3a3ebc71b3f6597bfed2
FelipeSutter <lipeps61@gmail.com>
2024-02-26 16:27:30 -0300

    added new math expressions

--- src/main/java/com/felipesutter/RestAPIWithSpringBoot3/MathController.java
+++ src/main/java/com/felipesutter/RestAPIWithSpringBoot3/MathController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.felipesutter.RestAPIWithSpringBoot3.exceptions.UnsupportedMathOperationException;
 
 @RestController
-@RequestMapping("/sum")
+@RequestMapping("/math")
 public class MathController {
 
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/{numberOne}/{numberTwo}")
+	@GetMapping("/sum/{numberOne}/{numberTwo}")
 	public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
 
 		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
@@ -23,6 +23,40 @@ public class MathController {
 		}
 
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+	
+	@GetMapping("/subtraction/{numberOne}/{numberTwo}")
+	public Double subtraction(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+
+		return convertToDouble(numberOne) - convertToDouble(numberTwo);
+	}
+	
+	@GetMapping("/multiplication/{numberOne}/{numberTwo}")
+	public Double multiplication(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+
+		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+	}
+	
+	@GetMapping("/division/{numberOne}/{numberTwo}")
+	public Double division(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+		
+		if (numberTwo.equals("0")) {
+			throw new UnsupportedMathOperationException("Not possible to divide for 0.");
+		}
+
+		return convertToDouble(numberOne) / convertToDouble(numberTwo);
 	}
 
 	private Double convertToDouble(String strNumber) {
