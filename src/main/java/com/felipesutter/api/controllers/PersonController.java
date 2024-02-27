9905117fb358ae5ec4bceaa53b17879834b07009
FelipeSutter <lipeps61@gmail.com>
2024-02-27 18:09:29 -0300

    refact: refatorando application.yml, controller e repository de person

--- src/main/java/com/felipesutter/api/controllers/PersonController.java
+++ src/main/java/com/felipesutter/api/controllers/PersonController.java
@@ -4,6 +4,7 @@ package com.felipesutter.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,9 +47,9 @@ public class PersonController {
 	}
 	
 	@DeleteMapping("/{id}")
-	public void delete(@PathVariable Long id){
-
+	public ResponseEntity<?> delete(@PathVariable Long id){
 		personService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/felipesutter/api/repositories/PersonRepository.java
+++ src/main/java/com/felipesutter/api/repositories/PersonRepository.java
@@ -1,11 +1,9 @@
 package com.felipesutter.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.felipesutter.api.model.Person;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
